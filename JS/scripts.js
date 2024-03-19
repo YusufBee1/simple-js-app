b4709d831ac8d73fafd256092df51966f8fe6f30
YusufBee1 <152460143+YusufBee1@users.noreply.github.com>
2024-03-18 19:35:19 -0700

    Updated JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,23 @@
-alert('Hello world');
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+// Initial blank array
+let pokemonList = [];
+
+// Add first pokemon
+pokemonList.push({
+  name: 'Bulbasaur',
+  height: 7,
+  types: ['grass', 'poison']
+});
+
+// Add second pokemon
+pokemonList.push({
+  name: 'Charmander',
+  height: 6,
+  types: ['fire']
+});
+
+// Add third pokemon
+pokemonList.push({
+  name: 'Squirtle',
+  height: 5,
+  types: ['water']
+});
