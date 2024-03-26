79ce98fa20c4f64de07526b94408b48e6e6c612f
YusufBee1 <152460143+YusufBee1@users.noreply.github.com>
2024-03-25 18:44:00 -0700

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -21,3 +21,33 @@ pokemonList.push({
   height: 5,
   types: ['water']
 });
+
+// Begin table structure
+let output = "<table><tr><th>Name</th><th>Height</th><th>Additional Notes</th></tr>";
+
+// For loop to iterate over the pokemonList array
+for (let i = 0; i<pokemonList.length; i++) {
+  
+  output += "<tr>";
+
+  // Name
+  output += "<td>" + pokemonList[i].name + "</td>";
+  
+  // Height
+  output += "<td>" + pokemonList[i].height + "</td>";
+
+  // Additional Notes
+  if (pokemonList[i].height > 6) {
+    // If height is over 6, add "Wow, that's big!" to the output
+    output += "<td>Wow, that's big!</td>";
+  } else {
+    output += "<td></td>";
+  }
+
+  output += "</tr>";
+}
+
+output += "</table>";
+
+// Write the output in the document
+document.write(output);
